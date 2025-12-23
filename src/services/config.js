a5ac0526b4d09e992dd3643161be6e145ae9723a
Meli <6402365+meliloop@users.noreply.github.com>
2025-12-23 14:19:56 -0300

    version

--- src/services/config.js
+++ src/services/config.js
@@ -1,4 +1,4 @@
-const apiUrl = process.env.NODE_ENV === 'development' ? 'http://localhost:9999' : 'https://api.swapmymood.org'
+const apiUrl = process.env.VUE_APP_API_URL || process.env.VUE_APP_BASE_API || (process.env.NODE_ENV === 'development' ? 'http://localhost:9999' : 'https://api.swapmymood.org')
 
 const configService = {
   apiUrl
